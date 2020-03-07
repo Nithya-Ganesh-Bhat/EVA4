5ac0a0dec02121de94c89967c75bdde356913223
Nithya-Ganesh-Bhat <61088699+Nithya-Ganesh-Bhat@users.noreply.github.com>
2020-03-08 00:28:03 +0530

    Update Model.py

--- S7/Model.py
+++ S7/Model.py
@@ -25,40 +25,40 @@ class Net(nn.Module):
 
         # Convolution Block 2
         self.convblock2=nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), padding=1, bias=False), #RF 14
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), padding=1, bias=False), #RF 10
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Dropout2d(self.drop_val),
-            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(3,3), padding=1, bias=False),   # RF 18
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(3,3), padding=1, bias=False),   # RF 14
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Dropout2d(self.drop_val),
         )
         self.MP2 = nn.Sequential(
-            nn.MaxPool2d(2,2),  # RF 20
+            nn.MaxPool2d(2,2),  # RF 16
             nn.Conv2d(in_channels=256, out_channels=64, kernel_size=(1,1), bias=False)
         )
 
          # Convolution Block 3
         self.convblock3=nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), padding=1,groups = 64, bias=False), #RF 28
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), padding=1,groups = 64, bias=False), #RF 24
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Dropout2d(self.drop_val),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), padding=2,dilation=2, bias=False),   # RF 36
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), padding=2,dilation=2, bias=False),   # RF 40
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Dropout2d(self.drop_val),
         )
 
         self.MP3 = nn.Sequential(
-            nn.MaxPool2d(2,2),  # RF 40
+            nn.MaxPool2d(2,2),  # RF 44
             nn.Conv2d(in_channels=256, out_channels=32, kernel_size=(1,1), bias=False)
         )
 
          # Convolution Block 4
         self.convblock4=nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=256, kernel_size=(3,3),groups=32, padding=1, bias=False), #RF 56
+            nn.Conv2d(in_channels=32, out_channels=256, kernel_size=(3,3),groups=32, padding=1, bias=False), #RF 60
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Dropout2d(self.drop_val),
